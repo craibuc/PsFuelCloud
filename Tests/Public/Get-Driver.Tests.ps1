a9edb3413fff1ebc6cd71268af85e5cdbe3b4161
Craig Buchanan <craig.buchanan@cogniza.com>
2020-05-11 16:05:58 -0500

    Improving unit tests
    Adding parameter validation
    Adding tests for paging
    Adding tests for single driver
    Correcting test for invalid credentials

--- Tests/Public/Get-Driver.Tests.ps1
+++ Tests/Public/Get-Driver.Tests.ps1
@@ -1,75 +1,168 @@
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+# /PsFuelCloud
+$ProjectDirectory = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
+
+# /PsFuelCloud/PsFuelCloud/Public
+$PublicPath = Join-Path $ProjectDirectory "/PsFuelCloud/Public/"
+
+# /PsFuelCloud/Tests/Fixtures/
+$FixturesDirectory = Join-Path $ProjectDirectory "/Tests/Fixtures/"
+
+# Get-Driver.ps1
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-. "$here\$sut"
+
+# . /PsFuelCloud/PsFuelCloud/Public/Get-Driver.ps1
+. Join-Path $PublicPath $sut
 
 Describe "Get-Driver" -tag 'unit' {
 
-    $AccessToken = ''
 
-    Context "Valid credentials supplied" {
+    Context "Parameter validation" {
+        $Command = Get-Command 'Get-Driver'
 
-        It "returns the list of drivers" {
+        Context 'AccessToken' {
+            $ParameterName = 'AccessToken'
 
-            $request = @{
+            It "is a [String]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+        Context 'Id' {
+            $ParameterName = 'Id'
+
+            It "is an [int]" {
+                $Command | Should -HaveParameter $ParameterName -Type int
+            }
+            It "is optional" {
+                $Command | Should -HaveParameter $ParameterName -Not -Mandatory
+            }
+
+        }
+
+    } # /context (parameter validation)
+
+    Context 'Default parameters' {
+
+        # arrange
+        $AccessToken = 'BEARER 01234567897abcdefghijklmnopqurtuvwxyz'
+
+        BeforeEach {
+
+            Mock Invoke-WebRequest {
+                Write-Debug "***** Page 1 *****"
+                $Fixture = 'Get-Driver.Multiple.1.json'
+                $Content = Get-Content (Join-Path $FixturesDirectory $Fixture) -Raw
+
+                $Response = New-MockObject -Type  Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject
+                $Response | Add-Member -Type NoteProperty -Name 'Content' -Value $Content -Force
+                $Response
+            }
+
+            Mock Invoke-WebRequest {
+                Write-Debug "***** Page 0 *****"
+                $Fixture = 'Get-Driver.Multiple.0.json'
+                $Content = Get-Content (Join-Path $FixturesDirectory $Fixture) -Raw
+
+                $Response = New-MockObject -Type  Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject
+                $Response | Add-Member -Type NoteProperty -Name 'Content' -Value $Content -Force
+                $Response
+            } -ParameterFilter { $Uri -eq 'https://api.fuelcloud.com/rest/v1.0/driver' }
+
+            # act
+            $Actual = Get-Driver -AccessToken $AccessToken
+        }
+
+        it "makes the correct request" {
+
+            # arrange
+            $Expected = @{
                 Uri='https://api.fuelcloud.com/rest/v1.0/driver'
                 Headers = @{
-                    Authorization=$AccessToken
                     "Content-Type"='application/json'
                 }
             }
 
-            # arrange
-            $response = @(
-                # page one
-                @{ 
-                    StatusCode="200"
-                    Content = @{
-                        data = @()
-                        meta = @{
-                            previous=$null
-                            total=100
-                            per_page=50
-                            next='https://api.fuelcloud.com/rest/v1.0/driver?page%5Bnumber%5D=2'
-                        }
-                    }
-                }
-                # page two
-                @{ 
-                    StatusCode="200"
-                    Content = @{
-                        data = @()
-                        meta = @{
-                            previous='https://api.fuelcloud.com/rest/v1.0/driver?page%5Bnumber%5D=1'
-                            total=100
-                            per_page=50
-                            next=$nuoo
-                        }
-                    }
-                }
-            )
-
-            Mock Invoke-WebRequest { returns $response[0] }
-            Mock Invoke-WebRequest { returns $response[1] }
-
-            #Assert
-            Assert‐MockCalled Invoke-WebRequest -Times 1 -Exactly
-
+            # assert
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter {
+                $Method -eq 'Get' -and
+                $Uri -eq $Expected.Uri
+                $ContentType -eq $Expected.Headers."Content-Type"
+                $Headers.Authorization -eq $AccessToken
+            } -Times 2 -Exactly
         }
-    } # /Context
+
+        It "returns the list of drivers" {
+            # assert
+            $Actual.length | Should -Be 4
+        }
+
+    } # /context (default parameters)
+
+    Context '-Id' {
+
+        # arrange
+        $AccessToken = 'BEARER 01234567897abcdefghijklmnopqurtuvwxyz'
+        $Id = 123456
+
+        Mock Invoke-WebRequest {
+            $Fixture = 'Get-Driver.One.json'
+            $Content = Get-Content (Join-Path $FixturesDirectory $Fixture) -Raw
+
+            $Response = New-MockObject -Type  Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject
+            $Response | Add-Member -Type NoteProperty -Name 'Content' -Value $Content -Force
+            $Response
+        }
+
+        BeforeEach {
+            # act
+            $Actual = Get-Driver -AccessToken $AccessToken -Id $Id
+        }
+
+        it "makes the correct request" {
+
+            # arrange
+            $Expected = @{
+                Uri="https://api.fuelcloud.com/rest/v1.0/driver/$Id"
+                Headers = @{
+                    "Content-Type"='application/json'
+                }
+            }
+
+            # assert
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter {
+                $Method -eq 'Get' -and
+                $Uri -eq "$($Expected.Uri)/$Id"
+                $ContentType -eq $Expected.Headers."Content-Type"
+                $Headers.Authorization -eq $AccessToken
+            }  -Times 1 -Exactly
+        }
+
+        It "returns the specified driver" {
+            # assert
+            $Actual.id | Should -Be $Id
+        }
+
+    } # /context (id)
+
 
     Context "Invalid credentials supplied" {
 
-        # arrange
-        $response = @{ 
-            StatusCode="200"
-            Content = @{
-                error = @{message = "some error occurred"}
+        it "throws an exception" {
+            # arrange
+            $AccessToken = 'BEARER 01234567897abcdefghijklmnopqurtuvwxyz'
+
+            Mock Invoke-WebRequest { 
+                $Response = New-Object System.Net.Http.HttpResponseMessage 401
+                $Phrase = 'Response status code does not indicate success: 401 (Unauthorized).'
+                Throw New-Object Microsoft.PowerShell.Commands.HttpResponseException $Phrase, $Response
             }
+
+            # act/assert
+            { Get-Driver -AccessToken $AccessToken } | Should -Throw 'Response status code does not indicate success: 401 (Unauthorized).'
         }
-
-        Mock Invoke-WebRequest { returns $response }
-
-        # Assert‐MockCalled Invoke-WebRequest -Times 1 -Exactly
 
     } # /Context
 
