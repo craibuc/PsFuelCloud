0dab92f47828a9da1f237b2d887469e460019064
Craig Buchanan <craig.buchanan@cogniza.com>
2020-06-12 13:55:25 -0500

    Adding Symlink task

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,5 +1,13 @@
 FormatTaskName "-------- {0} --------"
 
+Task Symlink {
+    $Module='PsFuelCloud'
+    $Here = Get-Location
+    Push-Location ~/.local/share/powershell/Modules
+    ln -s "$Here/$Module" $Module
+    Pop-Location
+}
+
 Task default -depends Publish
 
 Task Publish {
