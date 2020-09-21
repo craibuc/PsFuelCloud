a2cd9890c66be5a64f2777849b44fac640ad9da2
Craig Buchanan <craig@Galadriel.local>
2020-09-21 12:33:35 -0500

    initial implementation

--- PsFuelCloud/Public/New-Driver.ps1
+++ PsFuelCloud/Public/New-Driver.ps1
@@ -1,3 +1,84 @@
+<#
+.SYNOPSIS
+Labore labore ea est sint deserunt excepteur cillum sit elit et eiusmod nisi est magna.
+
+.PARAMETER AccessToken
+
+.PARAMETER id
+The driver's ID.
+
+.PARAMETER full_name
+The driver's full name.
+
+.PARAMETER pin
+The driver's PIN.
+
+.PARAMETER phone
+The driver's phone.
+
+.PARAMETER code
+The driver's code.
+
+.LINK
+https://developer.fuelcloud.com/?version=latest#5cb824b2-7faf-46a2-98b1-698e79d4786e
+
+#>
 function New-Driver {
-    throw [System.NotImplementedException] "New-Driver function has not be implemented."
+    
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [string]$AccessToken,
+
+        [Parameter(Mandatory)]
+        [string]$full_name,
+
+        [Parameter()]
+        [string]$phone,
+
+        [Parameter()]
+        [int]$pin,
+
+        [Parameter()]
+        [string]$code
+    )
+
+    begin {
+
+        Write-Debug "$($MyInvocation.MyCommand.Name)"
+        Write-Debug "AccessToken: $AccessToken"
+        Write-Debug "full_name: $full_name"
+        Write-Debug "pin: $pin"
+        Write-Debug "phone: $phone"
+        Write-Debug "code: $code"
+        
+        $Uri = "https://api.fuelcloud.com/rest/v1.0/driver/"
+        Write-Debug "Uri: $Uri"
+    
+        $Headers = @{Authorization = $AccessToken}
+    }
+
+    process {
+    
+        $Body = @{}
+        if ($full_name) { $Body['full_name']=$full_name }
+        if ($pin) { $Body['pin']=$pin }
+        if ($phone) { $Body['phone']=$phone }
+        if ($code) { $Body['code']=$code }
+        if ($status) { $Body['status']=$status }
+    
+        # PATCH
+        $Content = ( Invoke-WebRequest -Uri $uri -Method Post -Body $Body -ContentType "application/json" -Headers $Headers ).Content | ConvertFrom-Json
+    
+        # returns PsCustomObject representation of object
+        if ( $Content.data ) { $Content.data }
+    
+        # otherwise raise an exception
+        elseif ($Content.error) { Write-Error -Message $Content.error.message }
+    
+    }
+
+    end {}
+
 }
--- Tests/Public/New-Driver.Tests.ps1
+++ Tests/Public/New-Driver.Tests.ps1
@@ -5,7 +5,7 @@ $ProjectDirectory = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -P
 $PublicPath = Join-Path $ProjectDirectory "/PsFuelCloud/Public/"
 
 # /PsFuelCloud/Tests/Fixtures/
-# $FixturesDirectory = Join-Path $ProjectDirectory "/Tests/Fixtures/"
+$FixturesDirectory = Join-Path $ProjectDirectory "/Tests/Fixtures/"
 
 # New-Driver.ps1
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
@@ -14,7 +14,119 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . (Join-Path $PublicPath $sut)
 
 Describe "New-Driver" -tag 'unit' {
-    It "does something useful" -Skip {
-        $true | Should -Be $false
+
+    Context "Parameter validation" {
+        BeforeAll {
+            $Command = Get-Command 'New-Driver'
+        }
+
+        Context 'AccessToken' {
+            BeforeEach {
+                $ParameterName = 'AccessToken'
+            }
+            It "is a [String]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+        Context 'full_name' {
+            BeforeEach {
+                $ParameterName = 'full_name'
+            }
+            It "is a [String]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            It "is mandatory" {
+                $Command | Should -HaveParameter $ParameterName -Mandatory
+            }
+        }
+
+        Context 'phone' {
+            BeforeEach {
+                $ParameterName = 'phone'
+            }
+            It "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            It "is optional" {
+                $Command | Should -HaveParameter $ParameterName -Not -Mandatory
+            }
+        }
+
+        Context 'pin' {
+            BeforeEach {
+                $ParameterName = 'pin'
+            }
+            It "is a [int]" {
+                $Command | Should -HaveParameter $ParameterName -Type int
+            }
+            It "is optional" {
+                $Command | Should -HaveParameter $ParameterName -Not -Mandatory
+            }
+        }
+
+        Context 'code' {
+            BeforeEach {
+                $ParameterName = 'code'
+            }
+            It "is a [string]" {
+                $Command | Should -HaveParameter $ParameterName -Type string
+            }
+            It "is optional" {
+                $Command | Should -HaveParameter $ParameterName -Not -Mandatory
+            }
+        }
+
+    } # /context (parameter validation)
+
+    Context "Default" {
+    
+        BeforeAll {
+
+            # arrange
+            $AccessToken = '7522a674-f71d-4c51-ba0c-c8cff735d6b5'
+
+            # should match the values in fixtures file
+            $Driver = @{
+                full_name = 'New Driver'
+                pin = 68888
+                code = 'D1121987'
+                phone = '503-379-0000'
+            }
+
+            Mock Invoke-WebRequest {
+                $Fixture = 'New-Driver.json'
+                $Content = Get-Content (Join-Path $FixturesDirectory $Fixture) -Raw
+
+                $Response = New-MockObject -Type  Microsoft.PowerShell.Commands.BasicHtmlWebResponseObject
+                $Response | Add-Member -Type NoteProperty -Name 'Content' -Value $Content -Force
+                $Response
+            }
+
+        }
+
+        It "creates the correct request and returns the expected response" {
+            # act
+            $Actual = New-Driver -AccessToken AccessToken @Driver
+
+            # assert 
+            Assert-MockCalled Invoke-WebRequest -ParameterFilter {
+                Write-Debug "Headers: $Headers"
+                $Method -eq 'Post' `
+                -and $Uri -eq 'https://api.fuelcloud.com/rest/v1.0/driver/' `
+                -and $Body.full_name -eq $Driver.full_name
+                # ` -and $Headers -eq @{Authorization = $AccessToken}
+            }
+
+            $Actual.count | Should -Be 1
+            $Actual.full_name | Should -Be $Driver.full_name
+            $Actual.code | Should -Be $Driver.code
+            $Actual.phone | Should -Be $Driver.phone
+            $Actual.pin | Should -Be $Driver.pin
+        }
+
     }
 }
