3e169674a0d8998c8809dc7b1fd8dfa08b8dfb72
Craig Buchanan <craig.buchanan@cogniza.com>
2020-10-28 10:08:56 -0500

    Adding ErrorAction continue removing legacy code Adding alias for compatibility

--- PsFuelCloud/PsFuelCloud.psm1
+++ PsFuelCloud/PsFuelCloud.psm1
@@ -2,18 +2,18 @@
 # load (dot-source) *.PS1 files, excluding unit-test scripts (*.Tests.*), and disabled scripts (__*)
 #
 
-@("$PSScriptRoot\Public\*.ps1","$PSScriptRoot\Private\*.ps1") | Get-ChildItem | 
+@("$PSScriptRoot\Public\*.ps1","$PSScriptRoot\Private\*.ps1") | 
+    Get-ChildItem  -ErrorAction 'Continue' | 
     Where-Object { $_.Name -like '*.ps1' -and $_.Name -notlike '__*' -and $_.Name -notlike '*.Tests*' } | 
     ForEach-Object {
 
         # dot-source script
-        # Write-Debug "Loading $($_.BaseName)"
         . $_
-
-        # export functions in the `Public` folder
-        # if ( (Split-Path( $_.Directory) -Leaf) -Eq 'Public' ) {
-        #     Write-Debug "Exporting $($_.BaseName)"
-        #     Export-ModuleMember $_.BaseName
-        # }
-
     }
+
+##
+# Create aliases for compatibility
+#
+
+Set-Alias -Name 'Get-Driver' -Value 'Get-FuelCloudDriver'
+Set-Alias -Name 'Set-Driver' -Value 'Set-FuelCloudDriver'
