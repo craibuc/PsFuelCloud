dc6e2a7c14effcb67f666381a2883b58051b7c98
Craig Buchanan <craig.buchanan@cogniza.com>
2021-11-04 07:47:25 -0500

    version 0.7.1
    revising function-import logic

--- PsFuelCloud/PsFuelCloud.psm1
+++ PsFuelCloud/PsFuelCloud.psm1
@@ -2,14 +2,21 @@
 # load (dot-source) *.PS1 files, excluding unit-test scripts (*.Tests.*), and disabled scripts (__*)
 #
 
-@("$PSScriptRoot\Public\*.ps1","$PSScriptRoot\Private\*.ps1") | 
-    Get-ChildItem  -ErrorAction 'Continue' | 
-    Where-Object { $_.Name -like '*.ps1' -and $_.Name -notlike '__*' -and $_.Name -notlike '*.Tests*' } | 
-    ForEach-Object {
+Get-ChildItem *.ps1 -Recurse -Filter '__*','*.Tests.*' | ForEach-Object {
 
-        # dot-source script
-        . $_
-    }
+    # dot-source script
+    . $_
+
+}
+
+# @("$PSScriptRoot\Public\*.ps1","$PSScriptRoot\Private\*.ps1") | 
+#     Get-ChildItem  -ErrorAction 'Continue' | 
+#     Where-Object { $_.Name -like '*.ps1' -and $_.Name -notlike '__*' -and $_.Name -notlike '*.Tests*' } | 
+#     ForEach-Object {
+
+#         # dot-source script
+#         . $_
+#     }
 
 ##
 # Create aliases for compatibility
