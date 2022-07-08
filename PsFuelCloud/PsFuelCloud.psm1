61d19dbfaf1c1e5850412e87983cfc62ab88cd1e
Craig Buchanan <craig.buchanan@cogniza.com>
2022-07-08 12:46:42 -0500

    incrementing to 0.7.2
    fixing function importation

--- PsFuelCloud/PsFuelCloud.psm1
+++ PsFuelCloud/PsFuelCloud.psm1
@@ -2,21 +2,14 @@
 # load (dot-source) *.PS1 files, excluding unit-test scripts (*.Tests.*), and disabled scripts (__*)
 #
 
-Get-ChildItem *.ps1 -Recurse -Filter '__*','*.Tests.*' | ForEach-Object {
+@("$PSScriptRoot\Public\*.ps1","$PSScriptRoot\Private\*.ps1") | 
+    Get-ChildItem  -ErrorAction 'SilentlyContinue' | 
+    Where-Object { $_.Name -like '*.ps1' -and $_.Name -notlike '__*' -and $_.Name -notlike '*.Tests*' } | 
+    ForEach-Object {
 
-    # dot-source script
-    . $_
-
-}
-
-# @("$PSScriptRoot\Public\*.ps1","$PSScriptRoot\Private\*.ps1") | 
-#     Get-ChildItem  -ErrorAction 'Continue' | 
-#     Where-Object { $_.Name -like '*.ps1' -and $_.Name -notlike '__*' -and $_.Name -notlike '*.Tests*' } | 
-#     ForEach-Object {
-
-#         # dot-source script
-#         . $_
-#     }
+        # dot-source script
+        . $_
+    }
 
 ##
 # Create aliases for compatibility
